107d08fe6aa0e6ba6e194d43d86268b432dfe2f5
Tonaion02 <antonioambrosiopub@gmail.com>
2024-09-06 20:15:31 +0200

    WSG: added a missing method to DenseBagGrid2D and added the concept of datamarker

--- krABMaga/src/engine/fields/dense_object_grid_2d_t.rs
+++ krABMaga/src/engine/fields/dense_object_grid_2d_t.rs
@@ -1,7 +1,9 @@
 use std::hash::Hash;
 use std::option::Option;
+use std::marker::PhantomData;
 
 use crate::engine::location::Int2D;
+use crate::engine::Component;
 
 
 
@@ -11,21 +13,36 @@ use crate::engine::location::Int2D;
 // T: this version doesn't implement double buffering
 // T: TODO add all the methods that are missing from 
 // T: this implementation
-pub struct DenseBagGrid2D<O: Eq + Hash + Clone + Copy> {
+#[derive(Component)]
+pub struct DenseBagGrid2D<O: Eq + Hash + Clone + Copy, M: Sized> {
     pub bags: Vec<Vec<O>>,
     pub width: i32,
     pub height: i32,
+
+    phantom: PhantomData<M>,
 }
 
-impl<O: Eq + Hash + Clone + Copy> DenseBagGrid2D<O> {
+impl<O: Eq + Hash + Clone + Copy, M: Sized> DenseBagGrid2D<O, M> {
 
-    pub fn new(width: i32, height: i32) -> DenseBagGrid2D<O> {
+    pub fn new(width: i32, height: i32) -> DenseBagGrid2D<O, M> {
         DenseBagGrid2D {
             bags: std::iter::repeat_with(Vec::new).take((width * height) as usize).collect(),
             width: width,
             height: height,
+
+            phantom: PhantomData,
         }
-    }    
+    }   
+
+    pub fn set_object_location(&mut self, object: O, loc: &Int2D) {
+        let index = self.compute_index(loc);
+        
+        if !self.bags[index].is_empty() {
+            self.bags[index].retain(|&obj| obj != object);
+        }
+
+        self.bags[index].push(object);
+    }
 
     pub fn get_objects(&self, loc: &Int2D) -> Option<Vec<O>> {
         let mut cloned_bag = Vec::new();
@@ -36,6 +53,7 @@ impl<O: Eq + Hash + Clone + Copy> DenseBagGrid2D<O> {
         } else {
             // T: TODO check if this has some sense
             // T: I think that Vec implement Clone function
+            // T: taken from the original implementation
             for elem in &self.bags[index] {
                 cloned_bag.push(*elem);
             }
